b0577cc15eb56a67b783c18745397b154344ed31
unknown <vidir17@ru.is>
2017-11-27 14:33:37 +0000

    sma breytingar2

--- vika1/main.cpp
+++ vika1/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
+    string c;
+    ifstream fin;
+
+    fin.open("dummyFile.txt");
+    if(fin.is_open())
+    {
+        while(!fin.eof())
+        {
+            getline(fin, c);
+            cout << c << endl;
+        }
+        fin.close();
+    }
+    else
+    {
+        cout << "Unable to read from file!" << endl;
+    }
+
     return 0;
 }
