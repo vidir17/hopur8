90f169e40d5d9991b8e2a7e7b6b1883f46b803ac
unknown <vidir17@ru.is>
2017-11-27 15:22:59 +0000

    Betra A) i)

--- vika1/main.cpp
+++ vika1/main.cpp
@@ -14,23 +14,13 @@ int main()
     if(fin.is_open())
     {
         do{
-        //while(val == 'y' || val == 'Y'){
-
         while(counter < 10){
-        //for(int i = 0; getline(fin, c); i++){
-        //while(!fin.eof())
-        //{
             getline(fin, c);
             cout << c << endl;
-        //}
         counter++;
-        //}
-
         }
         cout << "Do you wish to continue (y/n)?" << endl;
         cin >> val;
-        //while(val == 'y' || val == 'Y' || val == 'n' || val == 'N'){
-
         if(val == 'y' || val == 'Y'){
                 counter = 0;
 
@@ -38,8 +28,6 @@ int main()
         else if(val == 'n' || val == 'N'){
             run = false;
         }
-        //}
-
         }while(run);
 
     fin.close();
@@ -48,6 +36,5 @@ int main()
     {
         cout << "Unable to read from file!" << endl;
     }
-
     return 0;
 }
