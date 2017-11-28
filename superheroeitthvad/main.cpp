4b76e53999ef3ed31e9ca599852a0b0b77a0134a
unknown <vidir17@ru.is>
2017-11-28 14:50:52 +0000

    breytingar

--- superheroeitthvad/main.cpp
+++ superheroeitthvad/main.cpp
@@ -13,13 +13,17 @@ class SuperHero {
 
         string name;
         int age;
-        char superpower;
+        char superpower[];
 
     public:
         SuperHero();
         SuperHero(string name, int age, char superpower);
+
         friend istream& operator >> (istream& in, SuperHero& superhero);
         friend ostream& operator << (ostream& out, const SuperHero& superhero);
+        string get_name(); //skilar til baka fra private
+        int get_age();
+        char get_superpower(char input);
 
 
 
@@ -44,22 +48,56 @@ int main()
 SuperHero::SuperHero(){
     name = "";
     age = 0;
-    superpower = 'n';
+    superpower[0] = 'n';
 }
 
 SuperHero::SuperHero(string name, int age, char superpower){
     this->name = name;
     this->age = age;
-    this->superpower = superpower;
+    this->superpower[0] = superpower;
 
 }
+string SuperHero::get_name(){
 
+    return name;
+
+}
+int SuperHero::get_age(){
+
+    return age;
+
+}
+char SuperHero::get_superpower(char input){
+
+    if(input == 'f'){
+        superpower[0] == 'Flying';
+    }
+    else if(input == 'g'){
+        superpower[0] == 'Giant';
+    }
+    else if(input == 'h'){
+        superpower[0] == 'Hacker';
+    }
+    else if(input == 'n'){
+        superpower[0] == 'None';
+    }
+    else
+        superpower[0] == 'Weakling';
+
+    return superpower[0];
+
+}
 istream& operator >> (istream& in, SuperHero& superhero){
 
     cout << "Nafn: ";
     in >> superhero.name;
     cout << "Aldur: ";
     in >> superhero.age;
+    cout << "Type f for Flying" << endl;
+    cout << "Type g for Giant" << endl;
+    cout << "Type h for Hacker" << endl;
+    cout << "Type n for None" << endl;
+    cout << "please enter superpower..." << endl;
     cout << "Superpower: ";
     in >> superhero.superpower;
 
@@ -69,9 +107,25 @@ istream& operator >> (istream& in, SuperHero& superhero){
 }
 ostream& operator << (ostream& out, const SuperHero& superhero){
 
-    out << "Nafn: " << superhero.name << endl;
-    out << "Aldur: " << superhero.age << endl;
-    out << "Superpower: " << superhero.superpower << endl;
+
+        out << endl;
+    if(superhero.superpower[0] == 'f'){
+        out << superhero.name << " " << "(" << superhero.age << "): " << "Flying" << endl;
+    }
+    else if(superhero.superpower[0] == 'g'){
+        out << superhero.name << " " << "(" << superhero.age << "): " << "Giant" << endl;
+    }
+    else if(superhero.superpower[0] == 'h'){
+        out << superhero.name << " " << "(" << superhero.age << "): " << "Hacker" << endl;
+    }
+    else if(superhero.superpower[0] == 'n'){
+        out << superhero.name << " " << "(" << superhero.age << "): " << "None" << endl;
+    }
+    else{
+        out << superhero.name << " " << "(" << superhero.age << "): " << "Weakling" << endl;
+    }
+
+
 
     return out;
 
