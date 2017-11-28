d0fa1c83c350d941a0832eb7e0f52c0d873171b9
unknown <vidir17@ru.is>
2017-11-28 12:29:04 +0000

    dagur numer 2

---
+++ vika1BB/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+class SuperHeroes{
+private:
+    string name;
+    int age;
+    char ability;
+public:
+
+    SuperHeroes(){
+        name = "";
+        age = 0;
+        ability = 'n';
+    }
+
+
+};
+
+int main()
+{
+    SuperHeroes bjorgvin;
+
+    return 0;
+}
